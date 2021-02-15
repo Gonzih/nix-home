efd25e48f1dc21ebf9cadee0831e105d4cc7f580
Max Gonzih <gonzih@gmail.com>
2021-02-15 11:03:16 -0500

    ADd plugin for fish shell

--- nix/lib/files.nix
+++ nix/lib/files.nix
@@ -5,15 +5,16 @@ let
 in
 {
   home.file = {
-    ".vim".source              = ln dotfiles/.vim;
-    ".vimrc".source            = ln dotfiles/.vim/vimrc;
-    ".vifm".source             = ln dotfiles/.vifm;
-    ".config/mc".source        = ln dotfiles/.mc;
-    ".config/fish".source      = ln dotfiles/.fish;
-    ".xmonad".source           = ln dotfiles/.xmonad;
-    ".mutt".source             = ln dotfiles/.mutt;
-    ".gitignore.global".source = ln dotfiles/.gitignore.global;
-    ".tmux.conf".source        = ln dotfiles/.tmux.conf;
-    "bin".source               = ln dotfiles/.bin;
+    ".vim".source                       = ln dotfiles/.vim;
+    ".vimrc".source                     = ln dotfiles/.vim/vimrc;
+    ".vifm".source                      = ln dotfiles/.vifm;
+    ".config/mc".source                 = ln dotfiles/.mc;
+    ".config/fish".source               = ln dotfiles/.fish;
+    ".xmonad".source                    = ln dotfiles/.xmonad;
+    ".mutt".source                      = ln dotfiles/.mutt;
+    ".gitignore.global".source          = ln dotfiles/.gitignore.global;
+    ".tmux.conf".source                 = ln dotfiles/.tmux.conf;
+    "bin".source                        = ln dotfiles/.bin;
+    ".config/plugin-foreign-env".source = ln dotfiles/plugin-foreign-env;
   };
 }

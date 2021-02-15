5fa8bd177c27c90688c516553698e431603501b3
Max Gonzih <gonzih@gmail.com>
2021-02-15 14:58:16 -0500

    Add redshift

--- nix/lib/services.nix
+++ nix/lib/services.nix
@@ -6,4 +6,14 @@
     enableSshSupport = true;
     enableScDaemon = true;
   };
+
+  services.redshift = {
+    enable = true;
+    latitude = "43.648723";
+    longitude = "-79.357716";
+    temperature = {
+      day = 5500;
+      night = 2200;
+    };
+  };
 }

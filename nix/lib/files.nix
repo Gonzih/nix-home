be8ea2f4e56ff0e341d31a2f8dbde2a53dc164be
Max Gonzih <gonzih@gmail.com>
2021-02-15 10:19:44 -0500

    Cleanup config

--- nix/lib/files.nix
+++ nix/lib/files.nix
@@ -1,16 +1,19 @@
 { config, pkgs, ... }:
 
+let
+  ln = config.lib.file.mkOutOfStoreSymlink;
+in
 {
   home.file = {
-    ".vim".source              = config.lib.file.mkOutOfStoreSymlink dotfiles/.vim;
-    ".vimrc".source            = config.lib.file.mkOutOfStoreSymlink dotfiles/.vim/vimrc;
-    ".vifm".source             = config.lib.file.mkOutOfStoreSymlink dotfiles/.vifm;
-    ".config/mc".source        = config.lib.file.mkOutOfStoreSymlink dotfiles/.mc;
-    ".config/fish".source      = config.lib.file.mkOutOfStoreSymlink dotfiles/.fish;
-    ".xmonad".source           = config.lib.file.mkOutOfStoreSymlink dotfiles/.xmonad;
-    ".mutt".source             = config.lib.file.mkOutOfStoreSymlink dotfiles/.mutt;
-    ".gitignore.global".source = config.lib.file.mkOutOfStoreSymlink dotfiles/.gitignore.global;
-    ".tmux.conf".source        = config.lib.file.mkOutOfStoreSymlink dotfiles/.tmux.conf;
-    "bin".source               = config.lib.file.mkOutOfStoreSymlink dotfiles/.bin;
+    ".vim".source              = ln dotfiles/.vim;
+    ".vimrc".source            = ln dotfiles/.vim/vimrc;
+    ".vifm".source             = ln dotfiles/.vifm;
+    ".config/mc".source        = ln dotfiles/.mc;
+    ".config/fish".source      = ln dotfiles/.fish;
+    ".xmonad".source           = ln dotfiles/.xmonad;
+    ".mutt".source             = ln dotfiles/.mutt;
+    ".gitignore.global".source = ln dotfiles/.gitignore.global;
+    ".tmux.conf".source        = ln dotfiles/.tmux.conf;
+    "bin".source               = ln dotfiles/.bin;
   };
 }

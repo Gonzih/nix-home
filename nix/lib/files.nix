fcb122e8daa391ba851ee872e3e5b3f8d0a160a0
Max Gonzih <gonzih@gmail.com>
2021-02-14 21:45:44 -0500

    Cleanup

--- nix/lib/files.nix
+++ nix/lib/files.nix
@@ -2,35 +2,15 @@
 
 {
   home.file = {
-    ".vim" = {
-      source = dotfiles/.vim;
-    };
-    ".vimrc" = {
-      source = dotfiles/.vim/vimrc;
-    };
-    ".vifm" = {
-      source = dotfiles/.vifm;
-    };
-    ".config/mc" = {
-      source = dotfiles/.mc;
-    };
-    ".config/fish" = {
-      source = dotfiles/.fish;
-    };
-    ".xmonad" = {
-      source = dotfiles/.xmonad;
-    };
-    ".mutt" = {
-      source = dotfiles/.mutt;
-    };
-    ".gitignore.global" = {
-      source = dotfiles/.gitignore.global;
-    };
-    ".tmux.conf" = {
-      source = dotfiles/.tmux.conf;
-    };
-    "bin" = {
-      source = dotfiles/.bin;
-    };
+    ".vim".source              = dotfiles/.vim;
+    ".vimrc".source            = dotfiles/.vim/vimrc;
+    ".vifm".source             = dotfiles/.vifm;
+    ".config/mc".source        = dotfiles/.mc;
+    ".config/fish".source      = dotfiles/.fish;
+    ".xmonad".source           = dotfiles/.xmonad;
+    ".mutt".source             = dotfiles/.mutt;
+    ".gitignore.global".source = dotfiles/.gitignore.global;
+    ".tmux.conf".source        = dotfiles/.tmux.conf;
+    "bin".source               = dotfiles/.bin;
   };
 }

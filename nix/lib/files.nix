69e0d58e6d7d65c3a0370cc38bf4d3ed3f97cc18
Max Gonzih <gonzih@gmail.com>
2021-02-23 11:25:19 -0500

    Update rofi config

--- nix/lib/files.nix
+++ nix/lib/files.nix
@@ -5,19 +5,20 @@ let
 in
 {
   home.file = {
-    ".vim".source                       = ln dotfiles/.vim;
-    ".vimrc".source                     = ln dotfiles/.vim/vimrc;
-    ".config/nvim".source               = ln dotfiles/.vim;
-    ".vifm".source                      = ln dotfiles/.vifm;
-    ".config/mc".source                 = ln dotfiles/.mc;
-    ".config/fish".source               = ln dotfiles/.fish;
-    ".xmonad".source                    = ln dotfiles/.xmonad;
-    ".mutt".source                      = ln dotfiles/.mutt;
-    ".gitignore.global".source          = ln dotfiles/.gitignore.global;
-    ".tmux.conf".source                 = ln dotfiles/.tmux.conf;
-    "bin".source                        = ln dotfiles/.bin;
-    ".config/plugin-foreign-env".source = ln dotfiles/plugin-foreign-env;
-    ".cache/dein".source                = ln dotfiles/.dein;
-    ".config/rofi/config.rasi".source   = ln dotfiles/.config.rasi;
+    ".vim".source                                 = ln dotfiles/.vim;
+    ".vimrc".source                               = ln dotfiles/.vim/vimrc;
+    ".config/nvim".source                         = ln dotfiles/.vim;
+    ".vifm".source                                = ln dotfiles/.vifm;
+    ".config/mc".source                           = ln dotfiles/.mc;
+    ".config/fish".source                         = ln dotfiles/.fish;
+    ".xmonad".source                              = ln dotfiles/.xmonad;
+    ".mutt".source                                = ln dotfiles/.mutt;
+    ".gitignore.global".source                    = ln dotfiles/.gitignore.global;
+    ".tmux.conf".source                           = ln dotfiles/.tmux.conf;
+    "bin".source                                  = ln dotfiles/.bin;
+    ".config/plugin-foreign-env".source           = ln dotfiles/plugin-foreign-env;
+    ".cache/dein".source                          = ln dotfiles/.dein;
+    ".config/rofi/config.rasi".source             = ln dotfiles/.rofi/config.rasi;
+    ".config/rofi/gruvbox-common.inc.rasi".source = ln dotfiles/.rofi/gruvbox-common.inc.rasi;
   };
 }

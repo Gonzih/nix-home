112d6eb3a3fce08308f0c977bc2be6ad6c060b1f
Max Gonzih <gonzih@gmail.com>
2021-02-24 11:42:53 -0500

    Comment this out for now

--- nix/lib/overlays/overlays.nix
+++ nix/lib/overlays/overlays.nix
@@ -17,6 +17,25 @@
         vendorSha256 = "0k28rxnd0vyb6ljbi83bm1gl7j4r660a3ckjxnzc2qzwvfj69g53";
         subPackages = [ "." ];
       };
+      # metals-latest = pkgs.metals.overrideAttrs(old: rec {
+      #   pname = "metals";
+      #   version = "0.10.0";
+
+      #   deps = pkgs.stdenv.mkDerivation {
+      #     name = "${pname}-deps-${version}";
+      #     buildCommand = ''
+      #       export COURSIER_CACHE=$(pwd)
+      #       ${pkgs.coursier}/bin/coursier fetch org.scalameta:metals_2.12:${version} \
+      #       -r bintray:scalacenter/releases \
+      #       -r sonatype:snapshots > deps
+      #       mkdir -p $out/share/java
+      #       cp -n $(< deps) $out/share/java/
+      #     '';
+      #     outputHashMode = "recursive";
+      #     outputHashAlgo = "sha256";
+      #     outputHash     = "1v9br6nad6yhq9y1z4b9z6xdsjrgqh7wlxww7vp7ws28cg85mqyg";
+      #   };
+      # });
     })
   ];
 }
